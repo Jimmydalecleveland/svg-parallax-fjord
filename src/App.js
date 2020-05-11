ffd6fffab0796596d77f37fffd4d434fd17fb071
Jimmy DC <jimmydalecleveland@gmail.com>
2020-05-10 22:49:37 -0600

    First Prototype of SVG animated Fjord with parallax

--- src/App.js
+++ src/App.js
@@ -1,26 +1,145 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react'
+import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons'
+import './App.css'
+import hills from './assets/hills.svg'
+import trees from './assets/trees.svg'
+import ground from './assets/ground.svg'
+import Mountains from './Mountains'
+import Remnis from './Remnis'
+import cloud1 from './assets/cloud-1-dark.svg'
+import cloud2 from './assets/cloud-2-light.svg'
+import cloud3 from './assets/cloud-3-dark.svg'
+import cloud4 from './assets/cloud-4-light.svg'
+import cloud5 from './assets/cloud-5-light.svg'
+import wave1 from './assets/wave-1.svg'
+import wave2 from './assets/wave-2.svg'
+import wave3 from './assets/wave-3.svg'
 
 function App() {
+  const paraRef = useRef(null)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Parallax pages={2} ref={paraRef} config={{ duration: 0 }}>
+        <ParallaxLayer offset={0} speed={0}>
+          <section className="upper"></section>
+        </ParallaxLayer>
+
+        {/* Background Clouds */}
+        <ParallaxLayer offset={0.1} speed={-0.45}>
+          <img src={cloud2} className="cloud2" alt="cloud" />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.1} speed={-0.45}>
+          <img src={cloud4} className="cloud4" alt="cloud" />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={-0.4}>
+          <img src={cloud5} className="cloud5" alt="cloud" />
+        </ParallaxLayer>
+
+        {/* Mountains */}
+        <ParallaxLayer
+          offset={0.1}
+          speed={-0.5}
+          style={{
+            display: 'flex',
+            alignItems: 'flex-end',
+            justifyContent: 'center',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Mountains />
+        </ParallaxLayer>
+
+        {/* Foreground Clouds (in front of mountains) */}
+        <ParallaxLayer offset={0.1} speed={-0.4}>
+          <img src={cloud1} className="cloud1" alt="cloud" />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.1} speed={-0.4}>
+          <img src={cloud3} className="cloud3" alt="cloud" />
+        </ParallaxLayer>
+
+        {/* Hills */}
+        <ParallaxLayer
+          offset={0.1}
+          speed={-0.3}
+          style={{
+            display: 'flex',
+            alignItems: 'flex-end',
+            justifyContent: 'center',
+          }}
+        >
+          <img src={hills} alt="hills" />
+        </ParallaxLayer>
+
+        {/* Trees */}
+        <ParallaxLayer
+          offset={0.1}
+          speed={-0.2}
+          style={{
+            display: 'flex',
+            alignItems: 'flex-end',
+            justifyContent: 'center',
+          }}
+        >
+          <img src={trees} alt="trees" />
+        </ParallaxLayer>
+
+        {/* Ground */}
+        <ParallaxLayer
+          offset={0.1}
+          speed={-0.1}
+          style={{
+            display: 'flex',
+            alignItems: 'flex-end',
+            justifyContent: 'center',
+          }}
+        >
+          <img src={ground} alt="ground" />
+        </ParallaxLayer>
+
+        {/* Water */}
+        <ParallaxLayer offset={1} speed={0} factor={1}>
+          <section className="water">
+            <Remnis className="remnis" />
+          </section>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0.09}
+          speed={0}
+          style={{
+            display: 'flex',
+            alignItems: 'flex-end',
+            width: '120%',
+          }}
+        >
+          <img src={wave3} className="wave3" alt="sea waves" />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0.13}
+          speed={0}
+          style={{
+            display: 'flex',
+            alignItems: 'flex-end',
+          }}
+        >
+          <img src={wave2} className="wave2" alt="sea waves" />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0.13}
+          speed={0}
+          style={{
+            display: 'flex',
+            alignItems: 'flex-end',
+            width: '120%',
+          }}
+        >
+          <img src={wave1} className="wave1" alt="sea waves" />
+        </ParallaxLayer>
+      </Parallax>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Landscape.js
@@ -0,0 +1,454 @@
+import React from 'react'
+import { ParallaxLayer } from 'react-spring/renderprops-addons'
+
+const Landscape = () => (
+  <svg className="landscape" width={2560} height={1379} viewBox="0 0 2560 1379">
+    <defs>
+      <style>
+        {
+          '.prefix__cls-1{fill:#e1eaea}.prefix__cls-2{fill:#fff}.prefix__cls-5{fill:#594677}.prefix__cls-7{fill:#816299}.prefix__cls-8{fill:#441818}.prefix__cls-9{fill:#6b2727}.prefix__cls-10{fill:#f57200}.prefix__cls-11{fill:#f79428}.prefix__cls-12{fill:#b54131}.prefix__cls-13{fill:#e04e3f}.prefix__cls-14{fill:#eb7e35}.prefix__cls-15{fill:#efa052}.prefix__cls-16{fill:#f8cb3c}.prefix__cls-17{fill:#f9dc8a}.prefix__cls-19{fill:#30562e}.prefix__cls-20{fill:#677c48}'
+        }
+      </style>
+    </defs>
+    <title>{'all-but-water-and-sky'}</title>
+    <g id="prefix__Layer_2" data-name="Layer 2">
+      <ParallaxLayer offset={0} speed={1}>
+        <g id="prefix__hills">
+          <path
+            className="prefix__cls-1"
+            d="M1244.96 0L689.11 1094.28h1111.7L1244.96 0z"
+          />
+          <path
+            className="prefix__cls-2"
+            d="M1245.26.59l-.84 1.13 244.94 1092.56h311.45L1245.26.59z"
+          />
+          <path
+            className="prefix__cls-1"
+            d="M1531.35 178.78l-731.83 915.5h1463.66l-731.83-915.5z"
+          />
+          <path
+            d="M2099 1095.05c-241.59 0-288.21-605.23-581-605.46-320.57-.25-287.82 605.46-534.81 605.46"
+            fill="#2e2351"
+          />
+          <path
+            className="prefix__cls-2"
+            d="M2099.81 1080.41l163.37 13.87-731.83-915.5-.06.09-.19 223.39 568.71 678.15z"
+          />
+          <path
+            className="prefix__cls-1"
+            d="M2000.52 360.65l-438.37 733.63h876.74l-438.37-733.63z"
+          />
+          <path
+            d="M1884.32 1095.05H2099c-241.59 0-288.21-605.23-581-605.46-5.15 0-10.2.15-15.18.46z"
+            fill="#413368"
+          />
+          <path
+            className="prefix__cls-5"
+            d="M1351.54 1095.05c259.23 11.26 187.88-344.84 492.43-346 269-1 277.33 353.86 537.89 345.24"
+          />
+          <path
+            className="prefix__cls-1"
+            d="M848.18 266.63l-503.84 827.65h1007.69L848.18 266.63z"
+          />
+          <path
+            className="prefix__cls-2"
+            d="M847.55 267.68l302.8 826.6h201.68L848.18 266.63l-.63 1.05z"
+          />
+          <path
+            className="prefix__cls-5"
+            d="M425.26 1095.05c247 0 199.1-532.28 518.62-506.3 252.46 20.54 330.24 519.08 597.23 506.3"
+          />
+          <path
+            fill="none"
+            stroke="#000"
+            strokeMiterlimit={10}
+            d="M983.18 1095.05h1017.99"
+          />
+          <path
+            className="prefix__cls-2"
+            d="M2001.17 360.65l262.01 733.63h175.71l-437.72-733.63z"
+          />
+          <path
+            className="prefix__cls-7"
+            d="M1541.11 1095.05h-19.68q9.68.46 19.68 0zM1289 1095.05h232.41c-251.3-11.88-331.4-486.28-577.55-506.3-8.1-.66-16-.94-23.6-.9zM2381.86 1094.28h-18q8.81.3 18 0z"
+          />
+          <path
+            className="prefix__cls-7"
+            d="M1849.52 749.08l230.05 345.43 284.31-.22c-241.39-7.84-257.05-341.46-514.36-345.21z"
+          />
+        </g>
+      </ParallaxLayer>
+      <ParallaxLayer offset={1} speed={1}>
+        <g id="prefix__Layer_7" data-name="Layer 7">
+          <path
+            className="prefix__cls-8"
+            d="M1419.37 1051.61h16.35v37.13h-16.35z"
+          />
+          <path
+            className="prefix__cls-9"
+            d="M1429.68 1051.61h6.04v37.13h-6.04z"
+          />
+          <path
+            className="prefix__cls-10"
+            d="M1427.55 964.9l-48.25 87.28h96.49l-48.24-87.28z"
+          />
+          <path
+            className="prefix__cls-11"
+            d="M1427.55 964.9l29.84 87.28h18.4l-48.24-87.28z"
+          />
+          <path
+            className="prefix__cls-10"
+            d="M1427.55 949.67l-39.49 68.38h78.97l-39.48-68.38z"
+          />
+          <path
+            className="prefix__cls-11"
+            d="M1427.55 949.67l24.02 68.38h15.46l-39.48-68.38z"
+          />
+          <path
+            className="prefix__cls-10"
+            d="M1427.55 932.77l-31.68 54.85h63.35l-31.67-54.85z"
+          />
+          <path
+            className="prefix__cls-11"
+            d="M1427.55 932.77l17.37 54.85h14.3l-31.67-54.85z"
+          />
+        </g>
+        <g id="prefix__Layer_3" data-name="Layer 3">
+          <path
+            className="prefix__cls-8"
+            d="M644.24 992.29h44.91v101.99h-44.91z"
+          />
+          <path
+            className="prefix__cls-9"
+            d="M672.57 992.29h16.58v101.99h-16.58z"
+          />
+          <path
+            className="prefix__cls-12"
+            d="M666.7 754.11L534.17 993.86h265.05L666.7 754.11z"
+          />
+          <path
+            className="prefix__cls-13"
+            d="M666.7 754.11l81.98 239.75h50.54L666.7 754.11z"
+          />
+          <path
+            className="prefix__cls-12"
+            d="M666.7 712.25L558.24 900.1h216.91L666.7 712.25z"
+          />
+          <path
+            className="prefix__cls-13"
+            d="M666.7 712.25l66 187.85h42.45L666.7 712.25z"
+          />
+          <path
+            className="prefix__cls-12"
+            d="M666.7 665.82l-87.01 150.7H753.7l-87-150.7z"
+          />
+          <path
+            className="prefix__cls-13"
+            d="M666.7 665.82l47.73 150.7h39.27l-87-150.7z"
+          />
+          <path
+            className="prefix__cls-8"
+            d="M1714.06 1044.94h21.62v49.1h-21.62z"
+          />
+          <path
+            className="prefix__cls-9"
+            d="M1727.7 1044.94h7.98v49.1h-7.98z"
+          />
+          <path
+            className="prefix__cls-12"
+            d="M1724.87 930.26l-63.81 115.44h127.61l-63.8-115.44z"
+          />
+          <path
+            className="prefix__cls-13"
+            d="M1724.87 930.26l39.47 115.44h24.33l-63.8-115.44z"
+          />
+          <path
+            className="prefix__cls-12"
+            d="M1724.87 910.11l-52.22 90.44h104.44l-52.22-90.44z"
+          />
+          <path
+            className="prefix__cls-13"
+            d="M1724.87 910.11l31.77 90.44h20.45l-52.22-90.44z"
+          />
+          <path
+            className="prefix__cls-12"
+            d="M1724.87 887.75l-41.89 72.56h83.78l-41.89-72.56z"
+          />
+          <path
+            className="prefix__cls-13"
+            d="M1724.87 887.75l22.98 72.56h18.91l-41.89-72.56z"
+          />
+          <path
+            className="prefix__cls-8"
+            d="M1253.81 1011.09h36.32v82.49h-36.32z"
+          />
+          <path
+            className="prefix__cls-9"
+            d="M1276.72 1011.09h13.41v82.49h-13.41z"
+          />
+          <path
+            className="prefix__cls-12"
+            d="M1271.97 820.45l-107.19 193.92h214.38l-107.19-193.92z"
+          />
+          <path
+            className="prefix__cls-13"
+            d="M1271.97 820.45l66.31 193.92h40.88l-107.19-193.92z"
+          />
+          <path
+            className="prefix__cls-12"
+            d="M1271.97 786.59l-87.72 151.94h175.44l-87.72-151.94z"
+          />
+          <path
+            className="prefix__cls-13"
+            d="M1271.97 786.59l53.38 151.94h34.34l-87.72-151.94z"
+          />
+          <path
+            className="prefix__cls-12"
+            d="M1271.97 749.04l-70.37 121.89h140.74l-70.37-121.89z"
+          />
+          <path
+            className="prefix__cls-13"
+            d="M1271.97 749.04l38.61 121.89h31.76l-70.37-121.89z"
+          />
+        </g>
+        <g id="prefix__Layer_4" data-name="Layer 4">
+          <path
+            className="prefix__cls-8"
+            d="M949.54 1016.87h33.64v76.41h-33.64z"
+          />
+          <path
+            className="prefix__cls-9"
+            d="M970.76 1016.87h12.42v76.41h-12.42z"
+          />
+          <path
+            className="prefix__cls-14"
+            d="M966.36 839.44l-99.28 179.61h198.56l-99.28-179.61z"
+          />
+          <path
+            className="prefix__cls-15"
+            d="M966.36 839.44l61.42 179.61h37.86l-99.28-179.61z"
+          />
+          <path
+            className="prefix__cls-14"
+            d="M966.36 808.08l-81.25 140.73h162.5l-81.25-140.73z"
+          />
+          <path
+            className="prefix__cls-15"
+            d="M966.36 808.08l49.44 140.73h31.81l-81.25-140.73z"
+          />
+          <path
+            className="prefix__cls-14"
+            d="M966.36 773.3l-65.18 112.9h130.36l-65.18-112.9z"
+          />
+          <path
+            className="prefix__cls-15"
+            d="M966.36 773.3l35.76 112.9h29.42l-65.18-112.9z"
+          />
+          <path
+            className="prefix__cls-8"
+            d="M2106.88 1033.25h25.19v57.22h-25.19z"
+          />
+          <path
+            className="prefix__cls-9"
+            d="M2122.77 1033.25h9.3v57.22h-9.3z"
+          />
+          <path
+            className="prefix__cls-14"
+            d="M2119.48 899.62l-74.36 134.51h148.71l-74.35-134.51z"
+          />
+          <path
+            className="prefix__cls-15"
+            d="M2119.48 899.62l45.99 134.51h28.36l-74.35-134.51z"
+          />
+          <path
+            className="prefix__cls-14"
+            d="M2119.48 876.13l-60.85 105.4h121.7l-60.85-105.4z"
+          />
+          <path
+            className="prefix__cls-15"
+            d="M2119.48 876.13l37.03 105.4h23.82l-60.85-105.4z"
+          />
+          <path
+            className="prefix__cls-14"
+            d="M2119.48 850.08l-48.82 84.55h97.63l-48.81-84.55z"
+          />
+          <path
+            className="prefix__cls-15"
+            d="M2119.48 850.08l26.78 84.55h22.03l-48.81-84.55z"
+          />
+          <path
+            className="prefix__cls-8"
+            d="M604.44 1036.1h25.19v57.22h-25.19z"
+          />
+          <path className="prefix__cls-9" d="M620.33 1036.1h9.3v57.22h-9.3z" />
+          <path
+            className="prefix__cls-14"
+            d="M617.04 902.47l-74.36 134.52h148.71l-74.35-134.52z"
+          />
+          <path
+            className="prefix__cls-15"
+            d="M617.04 902.47l45.99 134.52h28.36l-74.35-134.52z"
+          />
+          <path
+            className="prefix__cls-14"
+            d="M617.04 878.99l-60.85 105.39h121.7l-60.85-105.39z"
+          />
+          <path
+            className="prefix__cls-15"
+            d="M617.04 878.99l37.03 105.39h23.82l-60.85-105.39z"
+          />
+          <path
+            className="prefix__cls-14"
+            d="M617.04 852.94l-48.82 84.55h97.63l-48.81-84.55z"
+          />
+          <path
+            className="prefix__cls-15"
+            d="M617.04 852.94l26.78 84.55h22.03l-48.81-84.55z"
+          />
+        </g>
+        <g id="prefix__Layer_5" data-name="Layer 5">
+          <path
+            className="prefix__cls-8"
+            d="M1798.47 1020.52h30.6v69.5h-30.6z"
+          />
+          <path
+            className="prefix__cls-9"
+            d="M1817.77 1020.52h11.3v69.5h-11.3z"
+          />
+          <path
+            className="prefix__cls-16"
+            d="M1813.77 858.22l-90.31 163.38h180.62l-90.31-163.38z"
+          />
+          <path
+            className="prefix__cls-17"
+            d="M1813.77 858.22l55.86 163.38h34.45l-90.31-163.38z"
+          />
+          <path
+            className="prefix__cls-16"
+            d="M1813.77 829.69l-73.91 128.02h147.82l-73.91-128.02z"
+          />
+          <path
+            className="prefix__cls-17"
+            d="M1813.77 829.69l44.97 128.02h28.94l-73.91-128.02z"
+          />
+          <path
+            className="prefix__cls-16"
+            d="M1813.77 798.05l-59.3 102.7h118.59l-59.29-102.7z"
+          />
+          <path
+            className="prefix__cls-17"
+            d="M1813.77 798.05l32.53 102.7h26.76l-59.29-102.7z"
+          />
+          <path
+            className="prefix__cls-8"
+            d="M224.02 1039.46h24.9V1096h-24.9z"
+          />
+          <path
+            className="prefix__cls-9"
+            d="M239.72 1039.46h9.19V1096h-9.19z"
+          />
+          <path
+            className="prefix__cls-16"
+            d="M236.46 907.42L163 1040.33h146.93l-73.47-132.91z"
+          />
+          <path
+            className="prefix__cls-17"
+            d="M236.46 907.42l45.45 132.91h28.02l-73.47-132.91z"
+          />
+          <path
+            className="prefix__cls-16"
+            d="M236.46 884.21l-60.12 104.14h120.25l-60.13-104.14z"
+          />
+          <path
+            className="prefix__cls-17"
+            d="M236.46 884.21l36.59 104.14h23.54l-60.13-104.14z"
+          />
+          <path
+            className="prefix__cls-16"
+            d="M236.46 858.47l-48.23 83.55h96.47l-48.24-83.55z"
+          />
+          <path
+            className="prefix__cls-17"
+            d="M236.46 858.47l26.47 83.55h21.77l-48.24-83.55z"
+          />
+        </g>
+        <g id="prefix__Layer_6" data-name="Layer 6">
+          <path
+            className="prefix__cls-8"
+            d="M1047.17 1028.8h28.73v65.25h-28.73z"
+          />
+          <path
+            className="prefix__cls-9"
+            d="M1065.29 1028.8h10.61v65.25h-10.61z"
+          />
+          <path
+            className="prefix__cls-12"
+            d="M1061.53 876.42l-84.78 153.38h169.57l-84.79-153.38z"
+          />
+          <path
+            className="prefix__cls-13"
+            d="M1061.53 876.42l52.45 153.38h32.34l-84.79-153.38z"
+          />
+          <path
+            className="prefix__cls-12"
+            d="M1061.53 849.64l-69.38 120.18h138.77l-69.39-120.18z"
+          />
+          <path
+            className="prefix__cls-13"
+            d="M1061.53 849.64l42.23 120.18h27.16l-69.39-120.18z"
+          />
+          <path
+            className="prefix__cls-12"
+            d="M1061.53 819.94l-55.66 96.41h111.32l-55.66-96.41z"
+          />
+          <path
+            className="prefix__cls-13"
+            d="M1061.53 819.94l30.54 96.41h25.12l-55.66-96.41z"
+          />
+        </g>
+      </ParallaxLayer>
+      <path
+        fill="#de8937"
+        d="M0 1090h2560v289H0z"
+        id="prefix__Layer_8"
+        data-name="Layer 8"
+      />
+      <g id="prefix__Layer_11" data-name="Layer 11">
+        <path
+          className="prefix__cls-8"
+          d="M1552.6 1007.39h36.23v82.28h-36.23z"
+        />
+        <path
+          className="prefix__cls-9"
+          d="M1575.45 1007.39h13.38v82.28h-13.38z"
+        />
+        <path
+          className="prefix__cls-19"
+          d="M1570.72 815.23l-106.92 193.43h213.83l-106.91-193.43z"
+        />
+        <path
+          className="prefix__cls-20"
+          d="M1570.72 815.23l66.14 193.43h40.77l-106.91-193.43z"
+        />
+        <path
+          className="prefix__cls-19"
+          d="M1570.72 781.46l-87.51 151.56h175.01l-87.5-151.56z"
+        />
+        <path
+          className="prefix__cls-20"
+          d="M1570.72 781.46l53.24 151.56h34.26l-87.5-151.56z"
+        />
+        <path
+          className="prefix__cls-19"
+          d="M1570.72 744l-70.2 121.58h140.39L1570.72 744z"
+        />
+        <path
+          className="prefix__cls-20"
+          d="M1570.72 744l38.51 121.58h31.68L1570.72 744z"
+        />
+      </g>
+    </g>
+  </svg>
+)
+
+export default Landscape
