f4cf3c718dd223028221ef986c3f6f8a1a9ceeab
Jimmy DC <jimmydalecleveland@gmail.com>
2020-06-19 10:42:46 -0600

    add emotion and update Moutain

--- src/Mountains.js
+++ src/Mountains.js
@@ -1,37 +1,58 @@
 import * as React from 'react'
+import styled from '@emotion/styled'
 
-function Mountains(props) {
+const StyledSvg = styled.svg`
+  svg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+  .cls-1 {
+    fill: #e1eaea;
+  }
+  .cls-2 {
+    fill: #fff;
+  }
+`
+
+function Mountains() {
   return (
-    <svg id="prefix__mountains" viewBox="0 0 2560 1440" {...props}>
-      <defs>
-        <style>{'.prefix__cls-1{fill:#e1eaea}.prefix__cls-2{fill:#fff}'}</style>
-      </defs>
-      <title>{'mountains-2'}</title>
-      <path
-        className="prefix__cls-1"
-        d="M1244.96 58.03L689.11 1152.31h1111.7L1244.96 58.03z"
-      />
-      <path
-        className="prefix__cls-2"
-        d="M1245.26 58.63l-.84 1.13 244.94 1092.55h311.45L1245.26 58.63z"
-      />
-      <path
-        className="prefix__cls-1"
-        d="M1531.35 236.82l-731.83 915.49h1463.66l-731.83-915.49z"
-      />
-      <path
-        className="prefix__cls-2"
-        d="M2099.81 1138.44l163.37 13.87-731.83-915.49-.06.08L1575 445.03l524.81 693.41z"
-      />
-      <path
-        className="prefix__cls-1"
-        d="M2000.52 418.68l-438.37 733.63h876.74l-438.37-733.63zM848.18 324.67l-503.84 827.64h1007.69L848.18 324.67z"
-      />
-      <path
-        className="prefix__cls-2"
-        d="M847.55 325.71l302.8 826.6h201.68L848.18 324.67l-.63 1.04zM2001.17 418.68l262.01 733.63h175.71l-437.72-733.63z"
-      />
-    </svg>
+    <StyledSvg
+      id="mountains"
+      xmlns="http://www.w3.org/2000/svg"
+      width="2560"
+      height="1000"
+      viewBox="0 0 2560 1000">
+      <polygon
+        className="cls-1"
+        points="1174.19 45.67 725.76 928.49 1622.62 928.49 1174.19 45.67" />
+      <polygon
+        className="cls-2"
+        points="1174.43 46.15 1173.76 47.06 1371.36 928.49 1622.62 928.49 1174.43
+    46.15" />
+      <polygon
+        className="cls-1"
+        points="1405.23 189.91 814.83 928.49 1995.64 928.49 1405.23 189.91" />
+      <polygon
+        className="cls-2"
+        points="1863.84 917.3 1995.64 928.49 1405.23 189.91 1405.18 189.97 1440.45
+    357.89 1863.84 917.3" />
+      <polygon
+        className="cls-1"
+        points="1783.74 336.63 1430.08 928.49 2137.39 928.49 1783.74 336.63" />
+      <polygon
+        className="cls-1"
+        points="854.09 260.78 447.61 928.49 1260.57 928.49 854.09 260.78" />
+      <polygon
+        className="cls-2"
+        points="853.58 261.62 1097.86 928.49 1260.57 928.49 854.09 260.78 853.58
+    261.62" />
+      <polygon
+        className="cls-2"
+        points="1784.26 336.63 1995.64 928.49 2137.39 928.49 1784.26 336.63" />
+    </StyledSvg>
   )
 }
 
