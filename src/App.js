0898ea648c7764417f1fdde9c30023541e3ab7ec
Jimmy DC <jimmydalecleveland@gmail.com>
2020-06-19 11:54:39 -0600

    move background calculation to root component

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const Foreground = styled.div`
 
 const Background = styled.main`
   background-color: #0087d5;
-  background: ${({ scrollY }) => `rgb(${scrollY / 2.75}, ${135 - scrollY / 17}, ${213 - scrollY / 5.4})`};
+  background: ${({ animatingBackground }) => animatingBackground};
   position: fixed;
   width: 2560px;
   height: 1000px;
@@ -26,6 +26,10 @@ const Background = styled.main`
 
 function App() {
   const scrollY = useScrollY()
+  const red = scrollY / 2.75
+  const green = 135 - scrollY / 17
+  const blue = 213 - scrollY / 5.4
+  const animatingBackground = `rgb(${red}, ${green}, ${blue})`
   // TODO: When we can benchmark, let's look into how we should use window scroll with state
   // const handleScroll = useCallback((event) => {
   //   console.log(event)
@@ -33,7 +37,7 @@ function App() {
 
   return (
     <div className="App">
-      <Background scrollY={scrollY}>
+      <Background animatingBackground={animatingBackground}>
         <Mountains style={{ transform: `translateY(${-scrollY * 0.3}px)` }} />
       </Background>
       <Foreground />
